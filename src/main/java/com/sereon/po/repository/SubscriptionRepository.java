736b9599df5b266a06a83c271ba9cf1e18d4fef2
Sereon <pgh1678@gmail.com>
2021-08-11 23:27:29 +0900

    수익합계 및 수익률 수정

--- src/main/java/com/sereon/po/repository/SubscriptionRepository.java
+++ src/main/java/com/sereon/po/repository/SubscriptionRepository.java
@@ -25,7 +25,7 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
     @Query("select s, i from Subscription s join IPO i on i = s.IPO.stockCode where s.userId=:userId")
     Page<Object[]> findAllByUserId(Pageable pageable,@Param("userId") String userId);
 
-    @Query("SELECT sum(s.sellPrice * s.assignAmt - s.fee - s.tax - s.interest) FROM Subscription s JOIN IPO i ON i = s.IPO.stockCode WHERE s.userId =:userId AND i.listedDate between coalesce(:fromDt,'20200101') and :toDt")
+    @Query("SELECT sum((coalesce(s.sellPrice,0)-coalesce(i.offeringPrice,0)) * coalesce(s.assignAmt,0) - coalesce(s.fee,0) - coalesce(s.tax,0) - coalesce(s.interest,0)) FROM Subscription s JOIN IPO i ON i = s.IPO.stockCode WHERE s.userId =:userId AND i.listedDate between coalesce(:fromDt,'20200101') and :toDt and coalesce(s.sellPrice,0) <> 0")
     Long findYTD(@Param("userId") String userId,@Param("fromDt") String fromDt, @Param("toDt") String toDt);
 
     @Query("SELECT max(s.deposit) FROM Subscription s JOIN IPO i ON i = s.IPO.stockCode WHERE s.userId =:userId AND i.listedDate between coalesce(:fromDt,'20200101') and :toDt")
