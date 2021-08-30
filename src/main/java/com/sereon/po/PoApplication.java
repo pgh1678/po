07447d09af2ee5dcfd8806ace32668d64f26519b
Sereon <pgh1678@gmail.com>
2021-08-30 14:10:23 +0900

    1. 세션타임 늘리기

--- src/main/java/com/sereon/po/PoApplication.java
+++ src/main/java/com/sereon/po/PoApplication.java
@@ -2,6 +2,10 @@ package com.sereon.po;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 @SpringBootApplication
 public class PoApplication {
@@ -10,4 +14,17 @@ public class PoApplication {
 		SpringApplication.run(PoApplication.class, args);
 	}
 
+	@Bean
+	public HttpSessionListener httpSessionListener(){
+		return new SessionListener();
+	}
+
+}
+
+class SessionListener implements HttpSessionListener {
+
+	@Override
+	public void sessionCreated(HttpSessionEvent se){
+		se.getSession().setMaxInactiveInterval(60*60*24);
+	}
 }
